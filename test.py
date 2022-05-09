e67ec725805387d8d419681e3a7d512d604e6693
AditMeh <aditmeh123@gmail.com>
2022-05-09 03:42:56 -0400

    started writing some tests, still need to write tests for mean computations

---
+++ test.py
@@ -0,0 +1,43 @@
+import unittest
+from cv2 import trace
+import torch
+from training.StatsTracker import StatsTracker
+class TestStatsTracker(unittest.TestCase):
+
+
+    # Make sure inititalization is correct
+    def test_init(self):
+        tracker = StatsTracker(16, 32)
+
+        self.assertEqual(16, tracker.mean_denom_train)
+        self.assertEqual(32, tracker.mean_denom_val)
+    
+    # Make sure val and train values are added properly
+    def test_add_train_value(self):
+        tracker = StatsTracker(32, 16)
+        tracker.update_curr_losses(64, None)
+        self.assertEqual(tracker.train_loss_curr, 64)
+    def test_add_val_value(self):
+        tracker = StatsTracker(32, 16)
+        tracker.update_curr_losses(None, 14)
+        self.assertEqual(tracker.val_loss_curr, 14)
+
+    # Test multiple additions
+    def test_add_multiple_train_values(self):
+        tracker = StatsTracker(32, 16)
+        for i in range(13):
+            tracker.update_curr_losses(i, None) # train loss
+        for j in range(21):
+            tracker.update_curr_losses(None, j) # Val loss
+
+        self.assertEqual(tracker.train_loss_curr, (12*(13))/2)
+
+        self.assertEqual(tracker.val_loss_curr, (20*(21))/2)
+
+    # Test mean computations
+    def test_val_loss_mean(self):
+        # TODO
+        raise NotImplementedError
+
+if __name__ == '__main__':
+    unittest.main()
